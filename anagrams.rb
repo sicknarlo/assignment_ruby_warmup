8070c18704f6efb022e8b87d040fa55f5deabe9e
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-05-25 11:48:52 -0400

    updated ruby assignments

---
+++ anagrams.rb
@@ -0,0 +1,6 @@
+=begin
+Write a method anagrams which returns an array of anagrams that can be made with the specified string. Assume the input is only a single word (e.g. "loot" not "William Shakespeare").
+=end
+
+def anagrams(word)
+  
